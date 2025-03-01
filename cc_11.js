7827378db93a864dafd7d81397dda4cfbeb86eb6
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-03-01 17:33:06 -0500

    Task 1 - "Created book Class"

---
+++ cc_11.js
@@ -0,0 +1,52 @@
+//Task 1 "Created Book Class"
+class Book {
+    constructor(title, author, isbn, copies) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.copies = copies;
+    }
+
+    getDetails() {
+        return `Title: ${this.title}, Author: ${this.author}, ISBN: ${this.isbn}, Copies: ${this.copies}`;
+    }
+
+    updateCopies(quantity) {
+        this.copies += quantity;
+    }
+}
+
+
+class Borrower {
+    constructor(name, borrowerId) {
+        this.name = name;
+        this.borrowerId = borrowerId;
+        this.borrowedBooks = [];
+    }
+
+    borrowBook(book) {
+        this.borrowedBooks.push(book);
+    }
+
+    returnBook(book) {
+        this.borrowedBooks = this.borrowedBooks.filter(b => b !== book);
+    }
+}
+
+
+const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5);
+console.log(book1.getDetails());
+// Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 5"
+
+book1.updateCopies(-1);
+console.log(book1.getDetails());
+// Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
+
+const borrower1 = new Borrower("Alice Johnson", 201);
+borrower1.borrowBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
+// Expected output: ["The Great Gatsby"]
+
+borrower1.returnBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
+// Expected output: []
